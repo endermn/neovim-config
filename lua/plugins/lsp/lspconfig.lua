78370b6b9c9f28991d77edb31b6c15a6bceda686
gevla <gevla@MacBookPro.fritz.box>
2025-09-26 20:17:32 +0300

    removed comments

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -177,53 +177,6 @@ return {
 			},
 		})
 
-		-- HACK: If using Blink.cmp Configure all LSPs here
-
-		-- ( comment the ones in mason )
-		-- local lspconfig = require("lspconfig")
-		-- local capabilities = require("blink.cmp").get_lsp_capabilities() -- Import capabilities from blink.cmp
-
-		-- Configure lua_ls
-		-- lspconfig.lua_ls.setup({
-		--     capabilities = capabilities,
-		--     settings = {
-		--         Lua = {
-		--             diagnostics = {
-		--                 globals = { "vim" },
-		--             },
-		--             completion = {
-		--                 callSnippet = "Replace",
-		--             },
-		--             workspace = {
-		--                 library = {
-		--                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-		--                     [vim.fn.stdpath("config") .. "/lua"] = true,
-		--                 },
-		--             },
-		--         },
-		--     },
-		-- })
-		--
-		-- -- Configure tsserver (TypeScript and JavaScript)
-		-- lspconfig.ts_ls.setup({
-		--     capabilities = capabilities,
-		--     root_dir = function(fname)
-		--         local util = lspconfig.util
-		--         return not util.root_pattern('deno.json', 'deno.jsonc')(fname)
-		--             and util.root_pattern('tsconfig.json', 'package.json', 'jsconfig.json', '.git')(fname)
-		--     end,
-		--     single_file_support = false,
-		--     on_attach = function(client, bufnr)
-		--         -- Disable formatting if you're using a separate formatter like Prettier
-		--         client.server_capabilities.documentFormattingProvider = false
-		--     end,
-		--     init_options = {
-		--         preferences = {
-		--             includeCompletionsWithSnippetText = true,
-		--             includeCompletionsForImportStatements = true,
-		--         },
-		--     },
-		-- })
 
 		-- Add other LSP servers as needed, e.g., gopls, eslint, html, etc.
 		lspconfig.clangd.setup({ capabilities = capabilities })
